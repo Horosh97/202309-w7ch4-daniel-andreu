d2c4b4dc237825f1c846e2d6614b43ee7ad42c92
Daniel Andreu <daniel1997ar@gmail.com>
2023-10-26 18:50:49 +0200

    Add SlothForm.tsx with initial structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,11 @@
+import SlothForm from "../SlothForm/SlothForm";
+
 export const App = (): React.ReactElement => {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <SlothForm></SlothForm>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/SlothForm/SlothForm.tsx
@@ -0,0 +1,5 @@
+const SlothForm = (): React.ReactElement => {
+  return <div></div>;
+};
+
+export default SlothForm;
