a31bfde200fd007700d97f419b62d125b036d3b1
Daniel Andreu <daniel1997ar@gmail.com>
2023-10-26 20:52:34 +0200

    Implement SlothForm with addSloth, onChangeData and onFormSubmit

--- src/components/SlothForm/SlothForm.tsx
+++ src/components/SlothForm/SlothForm.tsx
@@ -1,5 +1,79 @@
+import { useState } from "react";
+import { Sloth } from "../../types";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
 const SlothForm = (): React.ReactElement => {
-  return <div></div>;
+  const initialNewSloth: Sloth = {
+    id: 0,
+    name: "",
+    picture: "",
+    fingers: 0,
+  };
+
+  const [newSloth, setNewSloth] = useState(initialNewSloth);
+
+  const addSloth = async (sloth: Sloth) => {
+    const response = await fetch(`${apiUrl}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(sloth),
+    });
+
+    if (response.status !== 201) {
+      return;
+    }
+  };
+
+  const onChangeData = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewSloth((newSloth) => ({
+      ...newSloth,
+      [event.target.id]: event.target.value,
+    }));
+  };
+
+  const onFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    addSloth(newSloth);
+
+    setNewSloth(initialNewSloth);
+  };
+
+  return (
+    <form className="form" autoComplete="off" onSubmit={onFormSubmit}>
+      <div>
+        <label htmlFor="name">Nombre: </label>
+        <input
+          type="text"
+          id="name"
+          value={newSloth.name}
+          onChange={onChangeData}
+        />
+      </div>
+      <div>
+        <label htmlFor="picture">URL de la imagen: </label>
+        <input
+          type="text"
+          id="picture"
+          value={newSloth.picture}
+          onChange={onChangeData}
+        />
+      </div>
+      <div>
+        <label htmlFor="fingers">Número de dedos del perezoso: </label>
+        <input
+          type="text"
+          id="fingers"
+          value={newSloth.fingers}
+          onChange={onChangeData}
+        />
+      </div>
+      <button type="submit">Crear</button>
+    </form>
+  );
 };
 
 export default SlothForm;
